6733549ef54d5159ec21fbe2144f14d7bfdab2b4
bicto <victoriang510@gmail.com>
2017-08-24 09:34:28 -0700

    Initial Commit

---
+++ Project_2/src/Project_2.java
@@ -0,0 +1,14 @@
+
+public class Project_2 {
+//Victoria Ng
+	//August 24, 2017
+	// This class compares output using print and println
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.print("hello");
+System.out.println("hello 2");
+System.out.print("hello 3");
+	}
+
+}
