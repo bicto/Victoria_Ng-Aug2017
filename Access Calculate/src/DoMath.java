5616b412cf1de5a744e5e0628faf8488cc084ce6
bicto <victoriang510@gmail.com>
2017-09-06 10:49:30 -0700

    Tying methods: square, cube, average, beginning radians

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,13 @@
+/*Victoria Ng
+ * September 6, 2017
+ * This is the runner for the Calculate Library; it is used to test the methods in Calculate
+ */
+public class DoMath {
+
+	public static void main(String[] args) {
+		System.out.println(Calculate.square(-7));
+		System.out.println(Calculate.cube(-7));
+		System.out.println(Calculate.average(6.4, 8.0, 9.4));
+	}
+
+}
---
+++ Calculate Library/src/Calculate.java
@@ -0,0 +1,27 @@
+/*Victoria Ng
+ * September 6, 2017
+ * Our self written math library
+ * It contains a series of methods to do basic math functions.
+ */
+public class Calculate {
+
+	//square the input
+	public static int square (int operand) {
+		int squared = operand*operand;
+		return squared;
+	}
+	//cube the input
+	public static int cube (int operand) {
+		int cubed = operand*operand*operand;
+		return cubed;
+	
+	}
+	//average the input
+	public static double average (double operand1, double operand2, double operand3 ) {
+		double averaged = (operand1+operand2+operand3)/3;
+		return averaged;
+		
+	//converts radians to degrees
+		
+	}
+}
