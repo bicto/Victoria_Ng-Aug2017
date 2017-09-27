df7ea72a75141c9a69d8965e6b614823a39512f6
bicto <31085133+bicto@users.noreply.github.com>
2017-09-27 10:48:31 -0700

    part 4 done

--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -4,7 +4,7 @@
  * It contains a series of methods to do basic math functions.
  */
 public class Calculate {
-
+//part 1
 	//square the input
 	public static int square (int operand) {
 		int squared = operand*operand;
@@ -43,31 +43,22 @@ public class Calculate {
 	}
 	
 	public static String toImproperFrac (int number, int numerator, int denominator) {
-    	int toImproperFrac = (number*denominator + numerator);
-    	return toImproperFrac + "/" + denominator;
+		int toImproperFrac = (number*denominator + numerator);
+		return toImproperFrac + "/" + denominator;
 }
 
-//toMixedNum
-public static String toMixedNum (int numerator, int denominator) {
-    	int x= (numerator/denominator);
-    	int y = (numerator%denominator);
-    	String toMixedNum = x + " " + (y + "/" + denominator);
-    	return toMixedNum;
+	//toMixedNum
+	public static String toMixedNum (int numerator, int denominator) {
+		int x= (numerator/denominator);
+		int y = (numerator%denominator);
+		String toMixedNum = x + " " + (y + "/" + denominator);
+		return toMixedNum;
 }
 
-//foil
-public static String foil (int a, int b, int c, int d, String x) {
-    	int term1 = (a*c);
-    	int term2 = (a*d) + (b* c);
-    	int term3 = (b*d);
-    	String foil = term1 + x + "^2+" + term2 + x + "+" + term3;
-    	return foil;
-}
-/*
-//determines whether one integer is evenly divisible by another
-//parameters: two integers, returns a Boolean
-*/
-public static Boolean isDivisibleBy (int num1, int num2) {
+//part 2
+	//isDivisibleBy: determines whether one integer is evenly divisible by another
+	//parameters: two integers, returns a Boolean
+	public static Boolean isDivisibleBy (int num1, int num2) {
     	if (num2 == 0 || num1 == 0) {
                 	throw new IllegalArgumentException ("divisor : 0");
     	}
@@ -78,57 +69,52 @@ public static Boolean isDivisibleBy (int num1, int num2) {
     	}
 }
 
-/*
-//returns the value of the number passed, accepts a double and returns a double
-*/
-public static double absValue (double a) {
-    	if ( a>0) {
+	//absValue
+	//returns the value of the number passed, accepts a double and returns a double
+	public static double absValue (double a) {
+		if ( a>0) {
           	return a;
-    	} else {
-    		return a*-1;
+		} else {
+    			return a*-1;
     	}
 }
 
-/*
-//returns the greater of the values passed
-//accepts 2 doubles and returns a double
-*/
-public static double max (double num1, double num2) {
-    	if (num1>num2) {
-    		return num1;
-    } else {
+	//max
+	//returns the greater of the values passed
+	//accepts 2 doubles and returns a double
+	public static double max (double num1, double num2) {
+		if (num1>num2) {
+			return num1;
+		} else {
 		return num2;
 	}
 }
 
-/*
-//accepts 3 doubles, returns a double
-*/
-public static double max (double number1, double number2, double number3) {
-	if (number1 > number2 && number1 > number3) {
-		return number1;
-	} else if (number2 > number1 && number2 > number3) {
-	return number2;
-	} else {
-	return number3;
+	//max (overload)
+	//accepts 3 doubles, returns a double
+	public static double max (double number1, double number2, double number3) {
+		if (number1 > number2 && number1 > number3) {
+			return number1;
+		} else if (number2 > number1 && number2 > number3) {
+			return number2;
+		} else {
+			return number3;
 	}
 }
 
-/* 
-//returns the smaller of the 2 values passed
-//accepts 2 integers, returns an integer
-*/
-public static int min (int num1, int num2) {
-	if (num1 < num2) {
-	return num1;
-} else {
-	return num2;
-}
+	//min
+	//returns the smaller of the 2 values passed
+	//accepts 2 integers, returns an integer
+	public static int min (int num1, int num2) {
+		if (num1 < num2) {
+			return num1;
+		} else {
+			return num2;
+		}
 }
 
-/*
-//rounds a double to 2 decimal places, returns a double
-*/
+	//round2
+	//rounds a double to 2 decimal places, returns a double
 	public static double round2 (double value) {
 		value = (value*100) + 0.5;
 		value = (int) value;
@@ -137,6 +123,8 @@ public static int min (int num1, int num2) {
 		return value;
 	}
 
+//part 3
+	//exponent
 	// accepts a double and an integer and returns a double
 	// raises a value to a positive integer power
 	public static double exponent (double base, int power) { 
@@ -150,6 +138,7 @@ public static int min (int num1, int num2) {
 		return answer;
 	}
 	
+	//factorial
 	// accepts an integer and returns an integer
 	// returns the factorial of the value passed
 	public static int factorial (int a) { 
@@ -163,8 +152,9 @@ public static int min (int num1, int num2) {
 	    return answer;
 	}  
 	
-	public static boolean isPrime (int a) { // accepts an integer and returns a boolean
-		// determines whether or not an integer is prime
+	//isPrime
+	// accepts an integer and returns a boolean, determines whether or not an integer is prime
+	public static boolean isPrime (int a) { 
 	    boolean check;
 	    if (a > 1) {
 	    	for (int i = a - 1; i > 1; i--) {
@@ -179,8 +169,9 @@ public static int min (int num1, int num2) {
 	    return true;
 		}
 
-	public static int gcf (int num1, int num2) { // accepts two positive integers and returns an integer
-		// finds the greatest common factor of two integers
+	//gcf
+	// accepts two positive integers and returns an integer, finds the greatest common factor of two integers
+	public static int gcf (int num1, int num2) { 
 		while (num2 != 0) {
 			int c = num1;
 			num1 = num2;
@@ -189,8 +180,10 @@ public static int min (int num1, int num2) {
 		return (int)Calculate.absValue(num1);
 	}
 	
-	public static double sqrt (double a) { // accepts and returns a double
-		// returns an approximation of the square root of the value passed, accurate to two decimal places
+	//sqrt
+	// accepts and returns a double
+	// returns an approximation of the square root of the value passed, accurate to two decimal places
+	public static double sqrt (double a) { 
 		if (a < 0) {
 			throw new IllegalArgumentException ("negative a:" + a);
 		}
